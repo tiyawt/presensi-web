fd91f1da64d225056e5983bc146ca41df18bef97
tiyawt <tiyaland3@gmail.com>
2026-08-11 12:41:36 +0700

    Fix bootstrap app

--- api/index.php
+++ api/index.php
@@ -1,23 +1,6 @@
 <?php
 
-// Buat direktori temporary di Vercel (/tmp)
-$storageDirs = [
-    '/tmp/storage/app',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/framework/views',
-    '/tmp/storage/logs',
-];
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
-foreach ($storageDirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
-    }
-}
-
-// Set penunjuk storage path ke /tmp
-putenv('APP_STORAGE_PATH=/tmp/storage');
-$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
-
-// Jalankan Laravel
 require __DIR__ . '/../public/index.php';
