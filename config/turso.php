3a35a9cf38e866bf4f62b862640b63376142d2ac
tiyawt <tiyaland3@gmail.com>
2026-08-11 21:48:58 +0700

    fix: install darkterminal/turso-http-laravel

--- config/turso.php
+++ config/turso.php
@@ -1,13 +1,10 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Turso Database Configuration
-    |--------------------------------------------------------------------------
-    */
     'db' => [
         'url'          => env('DB_DATABASE', env('TURSO_DATABASE_URL')),
         'access_token' => env('DB_AUTH_TOKEN', env('TURSO_AUTH_TOKEN')),
     ],
+    'url'          => env('DB_DATABASE', env('TURSO_DATABASE_URL')),
+    'access_token' => env('DB_AUTH_TOKEN', env('TURSO_AUTH_TOKEN')),
 ];
