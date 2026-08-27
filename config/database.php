617e171ac728384e694219d46e7393f03d8c41c4
tiyawt <tiyaland3@gmail.com>
2026-08-27 10:25:45 +0700

    fix: separate local sqlite and production turso connection

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION') === 'libsql' ? 'sqlite' : env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
