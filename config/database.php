90d02b67fa05dbee4c10d6b79ad6db8c73ae12c1
tiyawt <tiyaland3@gmail.com>
2026-08-27 10:18:24 +0700

    fix: separate local sqlite and production turso connection

--- config/database.php
+++ config/database.php
@@ -33,6 +33,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
+            'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
@@ -153,7 +154,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
